f6462dc879a451f56299f243f840cd8e31fd46a4
tapiwa <tapiwambizvo@gmail.com>
2024-10-19 05:09:04 +0200

    servlet hello world

---
+++ src/main/java/com/tapiwa/learn/java/ee/App.java
@@ -0,0 +1,10 @@
+package com.tapiwa.learn.java.ee;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
