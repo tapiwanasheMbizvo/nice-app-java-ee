506df52df0951227aff37b809c3198ec41116778
tapiwa <tapiwambizvo@gmail.com>
2024-10-19 06:59:16 +0200

    removed web.xaml

--- src/main/java/com/tapiwa/learn/java/ee/App.java
+++ src/main/java/com/tapiwa/learn/java/ee/App.java
@@ -3,9 +3,6 @@ package com.tapiwa.learn.java.ee;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-/**
- * Hello world!
- */
 @ApplicationPath("api")
 public class App extends Application {
 
